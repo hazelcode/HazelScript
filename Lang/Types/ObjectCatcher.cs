8b3b4c5215a4e7ffdedc62e865a223e86f5131de
Hazel Rojas <lacosaqhabla@gmail.com>
2023-11-24 09:50:32 -0600

    Resolve dotnet error

--- Lang/Types/ObjectCatcher.cs
+++ Lang/Types/ObjectCatcher.cs
@@ -2,7 +2,7 @@ namespace HazelScript.Lang;
 
 public class ObjectCatcher {
     public ObjectProps properties { get; set; }
-    public string? class { get; set; }
+    public string? type { get; set; }
     public string? classAlias { get; set; }
     public object? returns { get; set; }
 }
