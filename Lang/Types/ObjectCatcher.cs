f6dfee4c43f5eca33f801f8a4d1836b99f53c824
Hazel Rojas <lacosaqhabla@gmail.com>
2023-11-24 09:42:18 -0600

    Add 'returns' property to ObjectCatcher

--- Lang/Types/ObjectCatcher.cs
+++ Lang/Types/ObjectCatcher.cs
@@ -4,4 +4,5 @@ public class ObjectCatcher {
     public ObjectProps properties { get; set; }
     public string? class { get; set; }
     public string? classAlias { get; set; }
+    public object? returns { get; set; }
 }
