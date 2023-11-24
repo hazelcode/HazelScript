00d89d152fd1c21916b3d246d29af2da08f23aea
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2023-11-23 23:00:21 -0600

    Implement Brain class, binary search and bubble method

---
+++ Lang/Brain.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace HazelScript.Lang;
+
+public class Brain {
+    public static void BubbleMethod(ref string[] ar){
+        bool flag = true;
+        for(int i = 0; i < ar.Length && flag; i++){
+            flag = false;
+            for(int j = 0; j < ar.Length-i-1; j++){
+                if(ar[j] > ar[j+1]){
+                    flag = true;
+                    int aux = ar[j];
+                    ar[j] = ar[j+1];
+                    ar[j+1] = aux;
+                }
+            }
+        }
+    }
+    public static bool BinarySearch(int[] ar, int search){
+        int low = 0, middle = 0, high = ar.Length-1;
+        bool flag = false;
+        while(low <= high){
+            middle = (high + low)/2;
+            if(ar[middle] == search){
+                flag = true;
+                break;
+            } else if(search < ar[middle]){
+                high = middle-1;
+            } else {
+                low = middle+1;
+            }
+        }
+        return flag;
+    }
+}
