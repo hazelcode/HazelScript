5e2f5faa5d73f6d0c1d18ebeb39c71c27e616e5c
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2023-12-17 14:36:36 -0600

    remove returrnss from LanguageExceptions

--- Exceptions/LanguageExceptions.cs
+++ Exceptions/LanguageExceptions.cs
@@ -3,40 +3,34 @@ using System;
 namespace HazelScript.Exceptions;
 
 public class LanguageExceptions {
-    public static LineNote PackageNotFoundException(int line, string package){
-        return new LineNote {
+    public static LineNote PackageNotFoundException(int line, string package) =>
+        new LineNote {
             message = "Package \"" + package + "\" not found. Did you included that package?",
             line = line
         };
-    }
-    public static LineNote ReferenceNotFoundException(int line, string reference){
-        return new LineNote {
+    public static LineNote ReferenceNotFoundException(int line, string reference) =>
+        new LineNote {
             message = "Reference \"" + reference + "\" not found. You must implement that reference.",
             line = line
         };
-    }
-    public static LineNote MethodsDoesntReturnValuesException(int line, string method, string value){
-        return new LineNote {
+    public static LineNote MethodsDoesntReturnValuesException(int line, string method, string value) =>
+        new LineNote {
             message = "Methods doesn't return values. Your \"" + method + "\" method returns " + value + ". To return a value, switch your method to a function (strings, integers, etc).",
             line = line
         };
-    }
-    public static LineNote PreambleAusenceException(){
-        return new LineNote {
+    public static LineNote PreambleAusenceException() =>
+        new LineNote {
             message = "A complete basic preamble block is needed.",
             line = 1
         };
-    }
-    public static LineNote LogicalPreambleDelimitationsException(int line,int start, int end){
-        return new LineNote {
+    public static LineNote LogicalPreambleDelimitationsException(int line,int start, int end) =>
+        new LineNote {
             message = "You have to order correctly your preamble. Your preamble start is at line " + start + ", and your preamble end is at line " + end + ".",
             line = line
         };
-    }
-    public LineNote ImportNotSpecified(int line){
-        return new LinNote {
+    public LineNote ImportNotSpecified(int line) =>
+        new LinNote {
             message = "The import is not specified. Check and add it.",
             line = line
         };
-    }
 }
